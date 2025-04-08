420100245380d5f83e94ecc318c98ad4a27a2534
shotikobeboshvili <beboshvilishotiko61@gmail.com>
2025-04-08 19:51:24 +0400

    Initial commit

---
+++ app/welcome/welcome.tsx
@@ -0,0 +1,24 @@
+import Header from "./components/Header";
+import Home from "./components/Home";
+import Education from "./components/Education";
+import Skills from "./components/Skills";
+import Projects from "./components/Projects";
+import Contact from "./components/Contact";
+import "./components/App.css";
+import Footer from "./components/footer";
+
+
+export function Welcome() {
+  return (
+    <body>
+    <Header/>
+    <Home/>
+    <Education/>
+    <Skills/>
+    <Projects/>
+    <Contact/>
+    <Footer/>
+    </body>
+  );
+}
+
